8e9dce7b73aebab6522fd8a0617fe5fa51d6bdfb
Max Manuel <maxcamina@gmail.com>
2021-05-03 13:36:12 -0500

    Agrega el header de la app

---
+++ src/components/Organisms/Header.jsx
@@ -0,0 +1,49 @@
+import React, { createRef } from "react";
+import { NavLink } from "react-router-dom";
+
+const menu = createRef();
+const toggleMenu = () => menu.current.classList.toggle("show");
+const removeToken = () => {
+    localStorage.removeItem("token");
+    window.location = "/login";
+};
+
+const Header = () => {
+    return (
+        <header className="main-header">
+            <div className="ed-grid s-grid-5 lg-grid-4">
+                <div className="s-cols-4 lg-cols-1 s-cross-center">
+                    <NavLink to="/">
+                        <img className="main-logo" src="logo_brain.svg" alt="Brain-Logo" />
+                    </NavLink>
+                </div>
+                <div className="s-cols-1 lg-cols-3 s-cross-center s-main-end">
+                    <nav className="main-menu" ref={menu}>
+                        <ul>
+                            <li>
+                                <NavLink exact to="/">
+                                    Inicio
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/especialidades">Especialidades</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/cursos">Cursos</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/profesores">Profesores</NavLink>
+                            </li>
+                            <li>
+                                <span onClick={() => removeToken()}>Cerra Sesión</span>
+                            </li>
+                        </ul>
+                    </nav>
+                    <div className="main-menu-toggle to-l" onClick={() => toggleMenu()}></div>
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
