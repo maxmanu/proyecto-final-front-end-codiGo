23b2df1faf890c7d1c69b9febf52f60648bbe8f7
Max Manuel <maxcamina@gmail.com>
2021-05-03 13:32:19 -0500

    Configuración de redux y todas sus dependencias

---
+++ src/redux/actionCreators.js
@@ -0,0 +1,85 @@
+import axios from "axios";
+import {
+    GET_ALL_COURSES,
+    GET_ALL_POSTS,
+    GET_ALL_SPECIALITIES,
+    GET_ALL_TEACHERS,
+    GET_COURSE,
+    GET_POST,
+    GET_SPECIALITY,
+    GET_FRAGMENT,
+} from "./actions";
+
+const API_URL = process.env.REACT_APP_API_URL;
+
+export const getAllPosts = () => (dispatch) => {
+    axios.get(`${API_URL}/posts`).then((response) => {
+        return dispatch({
+            type: GET_ALL_POSTS,
+            posts: response.data,
+        });
+    });
+};
+
+export const getAllSpecialities = () => (dispatch) => {
+    axios.get(`${API_URL}/especialidades`).then((response) => {
+        return dispatch({
+            type: GET_ALL_SPECIALITIES,
+            specificities: response.data,
+        });
+    });
+};
+
+export const getAllCourses = () => (dispatch) => {
+    axios.get(`${API_URL}/cursos`).then((response) => {
+        return dispatch({
+            type: GET_ALL_COURSES,
+            courses: response.data,
+        });
+    });
+};
+
+export const getAllTeachers = () => (dispatch) => {
+    axios.get(`${API_URL}/profesores`).then((response) => {
+        return dispatch({
+            type: GET_ALL_TEACHERS,
+            teachers: response.data,
+        });
+    });
+};
+
+export const getPost = (id) => (dispatch) => {
+    axios.get(`${API_URL}/posts/${id}`).then((response) => {
+        return dispatch({
+            type: GET_POST,
+            post: response.data,
+        });
+    });
+};
+
+export const getSpeciality = (id) => (dispatch) => {
+    axios.get(`${API_URL}/especialidades/${id}`).then((response) => {
+        return dispatch({
+            type: GET_SPECIALITY,
+            speciality: response.data,
+        });
+    });
+};
+
+export const getCourse = (id) => (dispatch) => {
+    axios.get(`${API_URL}/cursos/${id}`).then((response) => {
+        return dispatch({
+            type: GET_COURSE,
+            course: response.data,
+        });
+    });
+};
+
+export const getFragment = (id) => (dispatch) => {
+    axios.get(`${API_URL}/clases/${id}`).then((response) => {
+        return dispatch({
+            type: GET_FRAGMENT,
+            fragment: response.data,
+        });
+    });
+};
